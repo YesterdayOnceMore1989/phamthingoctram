c298019b4bae48fb54f4cbf3eebd990854b8b923
YesterdayOnceMore1989 <32479680+YesterdayOnceMore1989@users.noreply.github.com>
2018-02-01 13:35:34 +0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var data = {
-  id:10
-};
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decode = jwt.verify(token, '123abc');
-console.log('decode',decode);
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt)=>{
+  bcrypt.hash(password, salt, (err,hash)=>{
+    console.log(hash);
+  })
+});
+
+var hashedPassword = '$2a$10$SasVuUVYoMwGhs1Yg/2.b.MkByRs8cHWxQ3Cl.DTXCFNnW/Vx0Pyq';
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+  console.log(res);
+});
+
+
+
+
+// var data = {
+//   id:10
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decode = jwt.verify(token, '123abc');
+// console.log('decode',decode);
 
 // var message = "I am user number 3";
 //
